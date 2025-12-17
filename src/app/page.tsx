e4aeb6e010e458beecdf426cc9d82644916e7b61
sshaik8 <samiurrahman.shaikh@morningstar.com>
2025-12-17 17:17:33 +0530

    css updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,62 +17,9 @@ import SectionClientSay from "@/components/SectionClientSay";
 function PageHome() {
   return (
     <main className="nc-PageHome relative overflow-hidden">
-      {/* GLASSMOPHIN */}
       <BgGlassmorphism />
-      {/* <UsersListClient /> */}
-
-      <div className="container relative space-y-24 mb-24 lg:space-y-28 lg:mb-28">
-        {/* SECTION HERO */}
+      <div className="container relative">
         <SectionHero className="lg:pb-16" />
-
-        {/* SECTION 1 */}
-        {/* <SectionSliderNewCategories categories={DEMO_CATS} /> */}
-
-        {/* <SectionOurFeatures /> */}
-
-        {/* <SectionGridFeaturePlaces cardType="card2" /> */}
-
-        <SectionHowItWork />
-
-        {/* <div className="relative py-16">
-          <BackgroundSection className="bg-orange-50 dark:bg-black/20" />
-          <SectionSliderNewCategories
-            categories={DEMO_CATS_2}
-            categoryCardType="card4"
-            itemPerRow={4}
-            heading="Suggestions for discovery"
-            subHeading="Popular places to stay that Chisfis recommends for you"
-            sliderStyle="style2"
-          />
-        </div> */}
-
-        {/* <SectionSubscribe2 /> */}
-
-        {/* <div className="relative py-16">
-          <BackgroundSection className="bg-orange-50 dark:bg-black dark:bg-opacity-20 " />
-          <SectionGridAuthorBox />
-        </div>
-
-        <SectionGridCategoryBox />
-
-        <div className="relative py-16">
-          <BackgroundSection />
-          <SectionBecomeAnAuthor />
-        </div>
-
-        <SectionSliderNewCategories
-          heading="Explore by types of stays"
-          subHeading="Explore houses based on 10 types of stays"
-          categoryCardType="card5"
-          itemPerRow={5}
-        />
-
-        <SectionVideos />
-
-        <div className="relative py-16">
-          <BackgroundSection />
-          <SectionClientSay />
-        </div> */}
       </div>
     </main>
   );
