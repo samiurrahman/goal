12d13114d52683dd5c00d337d5fb8d639db12e2a
samiurrahman <samiurrahman.shaikh@gmail.com>
2025-12-20 16:23:12 +0530

    updaes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import SectionHero from '@/app/(server-components)/SectionHero';
 import BgGlassmorphism from '@/components/BgGlassmorphism';
-import { TaxonomyType } from '@/data/types';
-import SectionSliderNewCategories from '@/components/SectionSliderNewCategories';
-import SectionOurFeatures from '@/components/SectionOurFeatures';
-import BackgroundSection from '@/components/BackgroundSection';
-import SectionGridFeaturePlaces from '@/components/SectionGridFeaturePlaces';
-import SectionHowItWork from '@/components/SectionHowItWork';
-import SectionSubscribe2 from '@/components/SectionSubscribe2';
-import SectionGridAuthorBox from '@/components/SectionGridAuthorBox';
-import SectionGridCategoryBox from '@/components/SectionGridCategoryBox';
-import SectionBecomeAnAuthor from '@/components/SectionBecomeAnAuthor';
-import SectionVideos from '@/components/SectionVideos';
-import SectionClientSay from '@/components/SectionClientSay';
-
 function PageHome() {
   return (
     <main className="nc-PageHome relative overflow-hidden">
