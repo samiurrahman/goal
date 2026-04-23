8e2cfd0ac0853fb94280d3bef0d3f86ab5496216
samiurrahman <samiurrahman.shaikh@gmail.com>
2026-04-24 01:32:31 +0530

    css

--- src/data/taxonomies.ts
+++ src/data/taxonomies.ts
@@ -1,20 +1,7 @@
-import __taxonomies from './jsons/__taxonomies.json';
 import __stayTaxonomies from './jsons/__stayTaxonomies.json';
 import __experiencesTaxonomies from './jsons/__experiencesTaxonomies.json';
 import { TaxonomyType } from './types';
 import { Route } from '@/routers/types';
-
-const DEMO_CATEGORIES: TaxonomyType[] = __taxonomies.map((item) => ({
-  ...item,
-  taxonomy: 'category',
-  href: item.href as Route,
-}));
-
-const DEMO_TAGS: TaxonomyType[] = __taxonomies.map((item) => ({
-  ...item,
-  taxonomy: 'tag',
-  href: item.href as Route,
-}));
 
 //
 
@@ -32,4 +19,4 @@ const DEMO_EXPERIENCES_CATEGORIES: TaxonomyType[] = __experiencesTaxonomies.map(
   href: item.href as Route,
 }));
 
-export { DEMO_CATEGORIES, DEMO_TAGS, DEMO_STAY_CATEGORIES, DEMO_EXPERIENCES_CATEGORIES };
+export { DEMO_STAY_CATEGORIES, DEMO_EXPERIENCES_CATEGORIES };
