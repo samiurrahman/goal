02085223fdb24717b4831e8001fed149c8b6438c
samiurrahman <samiurrahman.shaikh@gmail.com>
2026-04-30 19:46:55 +0530

    ssr

--- src/app/[agentName]/page.tsx
+++ src/app/[agentName]/page.tsx
@@ -125,14 +125,14 @@ const AgentDetails = async ({ params }: AgentDetailsProps) => {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(agentSchema) }}
       />
-      <div className="relative z-20 mt-4">
+      <div className="relative z-20 my-4">
         <Breadcrumb
           items={[{ label: 'Home', href: '/' }, { label: agentDetails?.known_as ?? '' }]}
         />
       </div>
       <div className="nc-ListingStayDetailPage w-full min-h-screen">
-        <main className="relative z-10 grid grid-cols-1 lg:grid-cols-5 gap-6 w-full mt-4 mb-24 lg:mb-32 lg:items-stretch">
-          <div className="lg:col-span-5 mt-6">
+        <main className="relative z-10 grid grid-cols-1 lg:grid-cols-5 gap-6 w-full mb-24 lg:mb-32 lg:items-stretch">
+          <div className="lg:col-span-5">
             <section className="overflow-hidden rounded-3xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 shadow-lg">
               <div className="relative h-60 md:h-60 w-full">
                 <Image
